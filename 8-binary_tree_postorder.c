7f76d2ef24dc6a4b12b1a6d897a0d97893c95a81
David Surumen <davidsurumen@gmail.com>
2022-08-25 22:11:48 +0000

    task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - traverses in post-order
+ * @tree: pointer to the tree
+ * @func: pointer to function to call for each node
+ *
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
