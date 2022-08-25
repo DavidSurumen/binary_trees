53053d6ee7a6806e3df4b90ac3e9d63789cde387
David Surumen <davidsurumen@gmail.com>
2022-08-25 14:45:15 +0000

    fixed failing test

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,21 +15,18 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
-	binary_tree_t *child;
 
 	newNode = binary_tree_node(parent, value);
-	
+
 	if (!parent)
 		return (NULL);
 
-	if (!parent->left)
-		parent->left = newNode;
-	else
+	if (parent->left)
 	{
-		child = parent->left;
-		child->parent = newNode;
-		parent->left = newNode;
-		newNode->left = child;
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
 	}
+	parent->left = newNode;
+
 	return (newNode);
 }
